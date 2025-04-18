ad6d07ebba22603bb985fc32feef2128b9b0a8a9
Abel Teixeira <120480217+ttabelhaxd@users.noreply.github.com>
2025-04-18 12:16:15 +0100

    Setting up frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
