0aca1c9bfc790a9bc50b89d04f2e24b6929dfe29
TiagoAlb12 <t.albuquerque@ua.pt>
2025-05-07 13:28:02 +0100

    backend more more updates

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (id, name, type) VALUES (1, 'Tiago', 'DRIVER');
+INSERT INTO users (id, name, type) VALUES (2, 'Joana', 'OPERATOR');
+
+INSERT INTO stations (
+    id, name, latitude, longitude, status,
+    discount, discount_value, discount_start_time, discount_end_time, operator_id
+) VALUES (
+             1, 'Estação UA', 40.633, -8.659, 'AVAILABLE',
+             true, 0.10, '08:00:00', '18:00:00', 2
+         );
--- backend/src/main/resources/db/migration/V002__DATA.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id, name, type) VALUES (1, 'Tiago', 'DRIVER');
-INSERT INTO users (id, name, type) VALUES (2, 'Joana', 'OPERATOR');
-
-INSERT INTO stations (id, name, operator_id) VALUES (1, 'Estação UA', 2);
